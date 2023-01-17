91600c951f1b2c88555e7911330fd61d99d54bd5
Chris Wang <git@chriswang.tech>
2023-01-17 15:31:51 +0800

    added support the "M" prefix letter for FIN introduced on 1 January 2022

--- nric-generator.py
+++ nric-generator.py
@@ -12,23 +12,27 @@ def get_identification_no(identification):
     total_sum += current_product
   
   #if 'T' or 'G' total_sum plus 4
-  if ((identification[0] =='T') or (identification[0] =='G')) :
+  if (identification[0] in ('T','G')) :
     total_sum += 4
-    
+  
+  #if 'M' total_sum plus 3
+  if (identification[0] in ('M')) :
+    total_sum += 3
+
   remainder = total_sum % 11
     
-  # generate the last checksum character
-  if ((identification[0] =='S') or (identification[0] =='T')):
+  # generate the last checksum letter
+  if (identification[0] in ('S', 'T')):
     return identification+alpha_singaporean[remainder]
-  elif((identification[0] =='F') or (identification[0] =='G')):
+  elif(identification[0] in ('F', 'G', 'M')):
     return identification+alpha_foreigner[remainder]
 
 def main():
-  parser = argparse.ArgumentParser(description="Singapore\'s National Registration Identity Card (NRIC) number generation tool")
-  parser.add_argument('--prefixes',  '-p', nargs="+", default=['S', 'T', 'F', 'G'], help='The prefixes of NRIC / FIN to be generated. S or T for NRIC. F or G for FIN')
+  parser = argparse.ArgumentParser(description="Singapore NRIC and FIN number generation tool")
+  parser.add_argument('--prefixes',  '-p', nargs="+", default=['S', 'T', 'F', 'G', 'M'], help='The prefixes of NRIC / FIN to be generated. S/T for NRIC. F/G/M for FIN')
   parser.add_argument('--min', type=int, default=0, choices=range(0, 100), help='The MIN first two digits representing year of birth')
   parser.add_argument('--max', type=int, default=99, choices=range(0, 100), help='The MAX first two digits representing year of birth')
-  parser.add_argument('--num', '-n', type=int, default=99, help='Numebr of NRIC / FIN numbers to generate')
+  parser.add_argument('--num', '-n', type=int, default=99, help='Number of NRIC / FIN numbers to generate')
   args = vars(parser.parse_args())
   # print(args)
   generate(args["prefixes"], args["min"], args["max"], args["num"])
